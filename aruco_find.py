1c35e2d0213ee710b105761774901bb1eefe57ed
Ali A <66160329+ashrafyind@users.noreply.github.com>
2021-06-22 21:31:26 +1000

    tidied up

--- aruco_find.py
+++ aruco_find.py
@@ -3,6 +3,8 @@ import cv2.aruco as aruco
 import numpy as np
 import os
 import time
+import math
+
 def findArucoMarkers(img, markerSize = 6, totalMarkers=250, draw=True):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -37,13 +39,19 @@ def findArucoMarkers(img, markerSize = 6, totalMarkers=250, draw=True):
         return topRight, bottomRight, bottomLeft, topLeft
     else:
         return None, None, None, None
+
 cap = cv2.VideoCapture(0)
+
 while True:
+
     success, img = cap.read()
     tr, br, bl, tl = findArucoMarkers(img)
     print(tr, br, bl, tl)
+
     cv2.imshow('img',img)
+
     k = cv2.waitKey(30) & 0xff
+
     if k == 27:
         break
 
